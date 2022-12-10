8dd15f3a483d2f3262bbba1f03b0e0f436249547
Thistoo <asfaras1cg0!>
2022-12-10 15:15:02 +0900

    recommendation 파트 변수명 바꾸고 java 와 연결하기 위한 준비 작업

--- app/src/main/java/com/example/newbottomnavi_anti/Recommendation.java
+++ app/src/main/java/com/example/newbottomnavi_anti/Recommendation.java
@@ -1,5 +1,6 @@
 package com.example.newbottomnavi_anti;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
@@ -10,14 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
 public class Recommendation extends Fragment {
-
-    int[] images = new int[] {R.drawable.ic_baseline_electric_bike, R.drawable.ic_baseline_emoji_emotions, R.drawable.ic_baseline_home, R.drawable.ic_baseline_settings};
-
 
     public Recommendation() {
         // Required empty public constructor
@@ -30,45 +29,79 @@ public class Recommendation extends Fragment {
 
         Log.e("추천", "추천들어옴");
 
-        ImageView banner_a_1 = view.findViewById(R.id.img_recom_1);
-        ImageView banner_a_2 = view.findViewById(R.id.img_recom_2);
-        ImageView banner_a_3 = view.findViewById(R.id.img_recom_3);
-        ImageView banner_a_4 = view.findViewById(R.id.img_recom_4);
-//
-//        Button refresh = view.findViewById(R.id.refresh_recom);
-//
-//        imageId1 = (int)(Math.random() * images.length);
-//        if(rand1Recom==null){
-//            Log.e("rand1", "null 임...");
-//        }
-//        rand1Recom.setBackgroundResource(images[imageId1]);
-//        imageId2 = (int)(Math.random() * images.length);
-//        rand2Recom.setBackgroundResource(images[imageId2]);
-//        imageId3 = (int)(Math.random() * images.length);
-//        rand3Recom.setBackgroundResource(images[imageId3]);
-//        imageId4 = (int)(Math.random() * images.length);
-//        rand4Recom.setBackgroundResource(images[imageId4]);
+//        카메라, 갤러리 버튼
+//        TODO : 연결하기
+        Button camera_btn = view.findViewById(R.id.camera_recom);
+        Button gallery_btn = view.findViewById(R.id.gallery_recom);
 
+//        그렇게 불러온 사진들 띄워주기 (최대 4개)
+//        TODO : 클릭 시 없어져야 함
+        ImageView wys_1 = view.findViewById(R.id.img_whatsyourstle_1);
+        ImageView wys_2 = view.findViewById(R.id.img_whatsyourstle_2);
+        ImageView wys_3 = view.findViewById(R.id.img_whatsyourstle_3);
+        ImageView wys_4 = view.findViewById(R.id.img_whatsyourstle_4);
+        
+//        추천하기 버튼
+//        TODO : 클릭 시 python 코드와 연결되도록
+        Button recommed_btn = view.findViewById(R.id.recommend_btn);
 
-        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_1.jpg?shrink=330:330&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(banner_a_1);
-        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_2.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(banner_a_2);
-        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_3.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(banner_a_3);
-        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_4.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(banner_a_4);
+//        refresh 버튼
+//        TODO : 클릭 시 기존 정보 바탕으로 (연산 x) 다시 추천
+        ImageButton refresh_btn = view.findViewById(R.id.refresh_imgbtn);
 
-//        refresh.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                imageId1 = (int)(Math.random() * images.length);
-//                rand1Recom.setBackgroundResource(images[imageId1]);
-//                imageId2 = (int)(Math.random() * images.length);
-//                rand2Recom.setBackgroundResource(images[imageId2]);
-//                imageId3 = (int)(Math.random() * images.length);
-//                rand3Recom.setBackgroundResource(images[imageId3]);
-//                imageId4 = (int)(Math.random() * images.length);
-//                rand4Recom.setBackgroundResource(images[imageId4]);
-//            }
-//        });
+//        추천해주는 제품들 사진
+//        a, b, c, d 가 각 제품
+//        _1, _2 이런게 그 속에 속하는 부 사진들
+        ImageView img_recom_a = view.findViewById(R.id.img_recom_a);
+        ImageView img_recom_a_1 = view.findViewById(R.id.img_recom_a_1);
+        ImageView img_recom_b = view.findViewById(R.id.img_recom_b);
+        ImageView img_recom_b_1 = view.findViewById(R.id.img_recom_b_1);
+        ImageView img_recom_c = view.findViewById(R.id.img_recom_c);
+        ImageView img_recom_c_1 = view.findViewById(R.id.img_recom_c_1);
+        ImageView img_recom_d = view.findViewById(R.id.img_recom_d);
+        ImageView img_recom_d_1 = view.findViewById(R.id.img_recom_d_1);
 
+//        사진찍기
+        camera_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+        
+//        갤러리에서 가져오기
+        gallery_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+//        추천하기 (파이썬과 연결)
+        recommed_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        
+//        추천 새로고침
+        refresh_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_1.jpg?shrink=330:330&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_a);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_2.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_a_1);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_3.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_b);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_4.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_b_1);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_1.jpg?shrink=330:330&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_c);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_2.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_c_1);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_3.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_d);
+        Glide.with(this).load("https://img.danawa.com/prod_img/500000/325/105/img/5105325_4.jpg?shrink=500:500&_v=20190729141738").placeholder(R.drawable.ic_baseline_emoji_emotions).into(img_recom_d_1);
 
 
 
