955b92152590994807a69a41f550612dd44faa64
Thistoo <sunpretti@naver.com>
2022-12-11 12:08:09 +0900

    setting 페이지 실패.. personal recommendation 페이지만 만들어둔 상태 (xml 만 있음)

--- app/src/main/java/com/example/newbottomnavi_anti/SettingsFragment.java
+++ app/src/main/java/com/example/newbottomnavi_anti/SettingsFragment.java
@@ -1,28 +1,41 @@
 package com.example.newbottomnavi_anti;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
-import com.example.newbottomnavi_anti.databinding.FragmentSettingsBinding;
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-public class SettingsFragment extends Fragment {
-
-    private FragmentSettingsBinding binding;
-
-    @NonNull
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        binding = FragmentSettingsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        return root;
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
     }
 }
+
+// 이걸 하면 설정 페이지가 뜨긴 뜨는데 설정페이지 -> 다른 페이지로 넘어가면 계속 null 문제가 생깁니다..
+//public class SettingsFragment extends PreferenceFragmentCompat {
+//
+//    SharedPreferences pref;
+//    public void onCreate(Bundle savedInstancesState){
+//        super.onCreate(savedInstancesState);
+//
+//        addPreferencesFromResource(R.xml.root_preferences);
+//
+//        Log.e("세팅", "세팅들어옴");
+//
+//        pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+//
+//        boolean isSound= pref.getBoolean("sound", false);
+//        Toast.makeText(getActivity(), "소리알림" + isSound, Toast.LENGTH_SHORT).show();
+//    }
+//
+//    @Override
+//    public void onCreatePreferences(@Nullable Bundle savedInstanceState, String rootKey) {
+//        Log.e("세팅", "세팅 이상한 create");
+//    }
+//}
